a6862a6517a905a0421363ea3522b0df3553d718
David Mynors <hello@davidmyno.rs>
2020-03-20 19:04:29 +0000

    update old packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack')
+path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   target: 'node',
   entry: './src/index.js',
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'hyperline.js',
     libraryTarget: 'commonjs'
   },
